e732bbfbe3643f45efb3bcc56d869b239859e0cc
Jas <jastha6wdc@gmail.com>
2023-12-10 01:00:38 +0530

    got the coloring rigth(once)

--- main.js
+++ main.js
@@ -3,20 +3,78 @@ function randomColor() {
   let g = Math.floor(Math.random() * 255);
   let b = Math.floor(Math.random() * 255);
 
-  return "rgb(" + r + ", " + g + ", " + b + ")"; // at least this is working
+  return `rgb(${r}, ${g}, ${b})`; // at least this is working
 }
-function onMouseClick(square) {
-  //let squareBrick = document.getElementsByClassName(".square");  // no diff
-  let squareBrick = document.getElementsByName("squarees"); // no diff
-  let vertiBrick = document.getElementsByClassName(".vertical");
-  let horiBrick = document.getElementsByClassName(".horizontal");
-  const changeColor = randomColor();
 
-  while (true) {
-    alert(`the random color is: ${changeColor}`);
-    squareBrick.style.backgroundColor = changeColor; // typeError undefiened setting
-  }
+// //let squareBrick = document.getElementsByClassName(".square"); // no diff
+// let squareBrick = document.getElementsByName("squarees"); // no diff
+// let vertiBrick = document.getElementsByClassName(".vertical");
+// let horiBrick = document.getElementsByClassName(".horizontal");
+// let changeColor = randomColor();
+function onMouseClick() {
+  let Brick = document.getElementById();
+  Brick.style.backgroundColor = randomColor();
 }
+
+// function onMouseClick() {
+//  let squareBrick = document.getElementById("sq-1");
+//  squareBrick.style.backgroundColor = randomColor();
+//
+// }
+// function onMouseClick() {
+//   let squareBrick = document.getElementById("sq-2");
+//   squareBrick.style.backgroundColor = randomColor();
+// }
+
+// function onMouseClick() {
+//   let squareBrick = document.getElementById("sq-3");
+//   squareBrick.style.backgroundColor = randomColor();
+// }
+
+// function onMouseClick() {
+//   let squareBrick = document.getElementById("sq-4");
+//   squareBrick.style.backgroundColor = randomColor();
+// }
+// function onMouseClick() {
+//   let squareBrick = document.getElementById("sq-5");
+//   squareBrick.style.backgroundColor = randomColor();
+// }
+// function onMouseClick() {
+//   let squareBrick = document.getElementById("sq-6");
+//   squareBrick.style.backgroundColor = randomColor();
+// }
+// function onMouseClick() {
+//   let squareBrick = document.getElementById("sq-7");
+//   squareBrick.style.backgroundColor = randomColor();
+// }
+// function onMouseClick() {
+//   let rectangleBrick = document.getElementById("rec-1");
+//   rectangleBrick.style.backgroundColor = randomColor();
+// }
+// function onMouseClick() {
+//   let rectangleBrick = document.getElementById("rec-2");
+//   rectangleBrick.style.backgroundColor = randomColor();
+// }
+// function onMouseClick() {
+//   let rectangleBrick = document.getElementById("rec-3");
+//   rectangleBrick.style.backgroundColor = randomColor();
+// }
+// function onMouseClick() {
+//   let rectangleBrick = document.getElementById("rec-4");
+//   rectangleBrick.style.backgroundColor = randomColor();
+// }
+// function onMouseClick() {
+//   let rectangleBrick = document.getElementById("rec-5");
+//   rectangleBrick.style.backgroundColor = randomColor();
+// }
+// function onMouseClick() {
+//   let rectangleBrick = document.getElementById("rec-6");
+//   rectangleBrick.style.backgroundColor = randomColor();
+// }
+// function onMouseClick() {
+//   let rectangleBrick = document.getElementById("rec-7");
+//   rectangleBrick.style.backgroundColor = randomColor();
+// }
 
 // check if the cell is empty or not
 // function isCellEmpty(column, row) {
