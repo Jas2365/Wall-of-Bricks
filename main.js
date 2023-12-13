eb0200dd41d7550e447d85599621ab0e4c584ff1
Jas <jastha6wdc@gmail.com>
2023-12-13 16:28:14 +0530

    complete

--- main.js
+++ main.js
@@ -2,83 +2,363 @@ function randomColor() {
   let r = Math.floor(Math.random() * 250);
   let g = Math.floor(Math.random() * 250);
   let b = Math.floor(Math.random() * 250);
-  if (r <= 243 && g <= 243 && b <= 243) {
-    return `rgb(${r}, ${g}, ${b})`;
-  }
-}
-function onMouseClick() {
-  document
-    .getElementById("sq-1")
-    .addEventListener("click", function onClick(event) {
-      event.target.style.backgroundColor = randomColor();
-    });
-  document
-    .getElementById("sq-2")
-    .addEventListener("click", function onClick(event) {
-      event.target.style.backgroundColor = randomColor();
-    });
-  document
-    .getElementById("sq-3")
-    .addEventListener("click", function onClick(event) {
-      event.target.style.backgroundColor = randomColor();
-    });
-  document
-    .getElementById("sq-4")
-    .addEventListener("click", function onClick(event) {
-      event.target.style.backgroundColor = randomColor();
-    });
-  document
-    .getElementById("sq-5")
-    .addEventListener("click", function onClick(event) {
-      event.target.style.backgroundColor = randomColor();
-    });
-  document
-    .getElementById("sq-6")
-    .addEventListener("click", function onClick(event) {
-      event.target.style.backgroundColor = randomColor();
-    });
-  document
-    .getElementById("sq-7")
-    .addEventListener("click", function onClick(event) {
-      event.target.style.backgroundColor = randomColor();
-    });
-  document
-    .getElementById("rec-1")
-    .addEventListener("click", function onClick(event) {
-      event.target.style.backgroundColor = randomColor();
-    });
-  document
-    .getElementById("rec-2")
-    .addEventListener("click", function onClick(event) {
-      event.target.style.backgroundColor = randomColor();
-    });
-  document
-    .getElementById("rec-3")
-    .addEventListener("click", function onClick(event) {
-      event.target.style.backgroundColor = randomColor();
-    });
-  document
-    .getElementById("rec-4")
-    .addEventListener("click", function onClick(event) {
-      event.target.style.backgroundColor = randomColor();
-    });
-  document
-    .getElementById("rec-5")
-    .addEventListener("click", function onClick(event) {
-      event.target.style.backgroundColor = randomColor();
-    });
-  document
-    .getElementById("rec-6")
-    .addEventListener("click", function onClick(event) {
-      event.target.style.backgroundColor = randomColor();
-    });
-  document
-    .getElementById("rec-7")
-    .addEventListener("click", function onClick(event) {
-      event.target.style.backgroundColor = randomColor();
-    });
+  return `rgb(${r}, ${g}, ${b})`;
 }
 
+// fn to get the values of adjcent bricks
+function getValue(value, brickColor) {
+  brickSelf = document.getElementById(value);
+  brickColor = document.getElementById(value).className;
+
+  rightBrick = document.getElementById(value + 1);
+  toppBrick = document.getElementById(value - 6);
+  leftBrick = document.getElementById(value - 1);
+  bottomBrick = document.getElementById(value + 6);
+  // center
+  let center = [8, 9, 10, 11, 14, 15, 16, 17, 20, 21, 22, 23];
+  if (value < 30) {
+    for (let i = 0; i < center.length; i++) {
+      if (value == center[i]) {
+        right = rightBrick.className;
+        topp = toppBrick.className;
+        left = leftBrick.className;
+        bottom = bottomBrick.className;
+      }
+    }
+  }
+  // borders
+  let topBorder = [2, 3, 4, 5];
+  if (value > 1 && value < 7) {
+    for (let i = 0; i < topBorder.length; i++) {
+      if (value == topBorder[i]) {
+        right = rightBrick.className;
+        topp = "border";
+        left = leftBrick.className;
+        bottom = bottomBrick.className;
+      }
+    }
+  }
+  let leftBorder = [7, 13, 19];
+  if (value > 6 && value < 20) {
+    for (let i = 0; i < leftBorder.length; i++) {
+      if (value == leftBorder[i]) {
+        right = rightBrick.className;
+        topp = toppBrick.className;
+        left = "border";
+        bottom = bottomBrick.className;
+      }
+    }
+  }
+  let rightBorder = [12, 18, 24];
+  if (value > 11 && value < 25) {
+    for (let i = 0; i < rightBorder.length; i++) {
+      if (value == rightBorder[i]) {
+        right = "border";
+        topp = toppBrick.className;
+        left = leftBrick.className;
+        bottom = bottomBrick.className;
+      }
+    }
+  }
+  let bottomBorder = [26, 27, 28, 29];
+  if (value > 25 && value < 30) {
+    for (let i = 0; i < bottomBorder.length; i++) {
+      if (value == bottomBorder[i]) {
+        right = rightBrick.className;
+        topp = toppBrick.className;
+        left = leftBrick.className;
+        bottom = "border";
+      }
+    }
+  }
+  // corners
+
+  switch (value) {
+    case 1:
+      right = rightBrick.className;
+      topp = "border";
+      left = "border";
+      bottom = bottomBrick.className;
+
+      break;
+    case 6:
+      right = "border";
+      topp = "border";
+      left = leftBrick.className;
+      bottom = bottomBrick.className;
+      break;
+    case 25:
+      right = rightBrick.className;
+      topp = toppBrick.className;
+      left = "border";
+      bottom = "border";
+      break;
+    case 30:
+      right = "border";
+      topp = toppBrick.className;
+      left = leftBrick.className;
+      bottom = "border";
+      break;
+    default:
+      break;
+  }
+  switchingColor(
+    brickSelf,
+    brickColor,
+    right,
+    rightBrick,
+    topp,
+    toppBrick,
+    left,
+    leftBrick,
+    bottom,
+    bottomBrick,
+    value
+  );
+}
+function switchingColor(
+  brickSelf,
+  brickColor,
+  right,
+  rightBrick,
+  topp,
+  toppBrick,
+  left,
+  leftBrick,
+  bottom,
+  bottomBrick,
+  value
+) {
+  {
+    // see self and around
+    if (brickColor != "white") {
+      console.log("see self: " + brickColor);
+      console.log("see id: " + value);
+      if (right != "white") {
+        console.log("see right: " + right);
+        if (topp != "white") {
+          console.log("see topp: " + topp);
+          if (left != "white") {
+            console.log("see left: " + left);
+            if (bottom != "white") {
+              console.log("see bottom: " + bottom);
+              document.getElementById(value).style.backgroundColor =
+                randomColor();
+              console.log("see id: " + value);
+              console.log("see self: " + brickColor);
+              console.log("see right: " + right);
+              console.log("see topp: " + topp);
+              console.log("see left: " + left);
+              console.log("see bottom: " + bottom);
+            } else if (bottom == "border") {
+              document.getElementById(value).style.backgroundColor =
+                randomColor();
+              document.getElementById(value).classList.add("border");
+              console.log("see id: " + value);
+              console.log("see self: " + brickColor);
+              console.log("see right: " + right);
+              console.log("see topp: " + topp);
+              console.log("see left: " + left);
+              console.log("see bottom: " + bottom);
+            } else {
+              bottomBrick.style.backgroundColor = randomColor();
+              bottomBrick.className = brickColor;
+              bottomBrick = brickSelf;
+              document.getElementById(value).className = "white";
+              document.getElementById(value).style.backgroundColor = "white";
+              console.log("see id: " + value);
+              console.log("see self: " + brickColor);
+              console.log("see right: " + right);
+              console.log("see topp: " + topp);
+              console.log("see left: " + left);
+              console.log("see bottom: " + bottom);
+            }
+          } else if (left == "border") {
+            document.getElementById(value).style.backgroundColor =
+              randomColor();
+            document.getElementById(value).classList.add("border");
+          } else {
+            leftBrick.style.backgroundColor = randomColor();
+            leftBrick.className = brickColor;
+            leftBrick = brickSelf;
+            document.getElementById(value).className = "white";
+            document.getElementById(value).style.backgroundColor = "white";
+          }
+        } else if (topp == "border") {
+          document.getElementById(value).style.backgroundColor = randomColor();
+          document.getElementById(value).classList.add("border");
+        } else {
+          toppBrick.style.backgroundColor = randomColor();
+          toppBrick.className = brickColor;
+          toppBrick = brickSelf;
+          document.getElementById(value).className = "white";
+          document.getElementById(value).style.backgroundColor = "white";
+        }
+      } else if (right == "border") {
+        document.getElementById(value).style.backgroundColor = randomColor();
+        document.getElementById(value).classList.add("border");
+      } else {
+        rightBrick.style.backgroundColor = randomColor();
+        rightBrick.className = brickColor;
+        rightBrick = brickSelf;
+        document.getElementById(value).className = "white";
+        document.getElementById(value).style.backgroundColor = "white";
+      }
+    }
+  }
+}
+// function onMouseClick() {
+//   document
+//     .getElementById("1")
+//     .addEventListener("click", function onClick(event) {
+//       event.target.style.backgroundColor = randomColor();
+//     });
+//   document
+//     .getElementById("2")
+//     .addEventListener("click", function onClick(event) {
+//       event.target.style.backgroundColor = randomColor();
+//     });
+//   document
+//     .getElementById("3")
+//     .addEventListener("click", function onClick(event) {
+//       event.target.style.backgroundColor = randomColor();
+//     });
+//   document
+//     .getElementById("4")
+//     .addEventListener("click", function onClick(event) {
+//       event.target.style.backgroundColor = randomColor();
+//     });
+//   document
+//     .getElementById("5")
+//     .addEventListener("click", function onClick(event) {
+//       event.target.style.backgroundColor = randomColor();
+//     });
+//   document
+//     .getElementById("6")
+//     .addEventListener("click", function onClick(event) {
+//       event.target.style.backgroundColor = randomColor();
+//     });
+//   document
+//     .getElementById("7")
+//     .addEventListener("click", function onClick(event) {
+//       event.target.style.backgroundColor = randomColor();
+//     });
+//   document
+//     .getElementById("8")
+//     .addEventListener("click", function onClick(event) {
+//       event.target.style.backgroundColor = randomColor();
+//     });
+//   document
+//     .getElementById("9")
+//     .addEventListener("click", function onClick(event) {
+//       event.target.style.backgroundColor = randomColor();
+//     });
+//   document
+//     .getElementById("10")
+//     .addEventListener("click", function onClick(event) {
+//       event.target.style.backgroundColor = randomColor();
+//     });
+//   document
+//     .getElementById("11")
+//     .addEventListener("click", function onClick(event) {
+//       event.target.style.backgroundColor = randomColor();
+//     });
+//   document
+//     .getElementById("12")
+//     .addEventListener("click", function onClick(event) {
+//       event.target.style.backgroundColor = randomColor();
+//     });
+//   document
+//     .getElementById("13")
+//     .addEventListener("click", function onClick(event) {
+//       event.target.style.backgroundColor = randomColor();
+//     });
+//   document
+//     .getElementById("14")
+//     .addEventListener("click", function onClick(event) {
+//       event.target.style.backgroundColor = randomColor();
+//     });
+//   document
+//     .getElementById("15")
+//     .addEventListener("click", function onClick(event) {
+//       event.target.style.backgroundColor = randomColor();
+//     });
+//   document
+//     .getElementById("16")
+//     .addEventListener("click", function onClick(event) {
+//       event.target.style.backgroundColor = randomColor();
+//     });
+//   document
+//     .getElementById("17")
+//     .addEventListener("click", function onClick(event) {
+//       event.target.style.backgroundColor = randomColor();
+//     });
+//   document
+//     .getElementById("18")
+//     .addEventListener("click", function onClick(event) {
+//       event.target.style.backgroundColor = randomColor();
+//     });
+//   document
+//     .getElementById("19")
+//     .addEventListener("click", function onClick(event) {
+//       event.target.style.backgroundColor = randomColor();
+//     });
+//   document
+//     .getElementById("20")
+//     .addEventListener("click", function onClick(event) {
+//       event.target.style.backgroundColor = randomColor();
+//     });
+//   document
+//     .getElementById("21")
+//     .addEventListener("click", function onClick(event) {
+//       event.target.style.backgroundColor = randomColor();
+//     });
+//   document
+//     .getElementById("22")
+//     .addEventListener("click", function onClick(event) {
+//       event.target.style.backgroundColor = randomColor();
+//     });
+//   document
+//     .getElementById("23")
+//     .addEventListener("click", function onClick(event) {
+//       event.target.style.backgroundColor = randomColor();
+//     });
+//   document
+//     .getElementById("24")
+//     .addEventListener("click", function onClick(event) {
+//       event.target.style.backgroundColor = randomColor();
+//     });
+//   document
+//     .getElementById("25")
+//     .addEventListener("click", function onClick(event) {
+//       event.target.style.backgroundColor = randomColor();
+//     });
+//   document
+//     .getElementById("26")
+//     .addEventListener("click", function onClick(event) {
+//       event.target.style.backgroundColor = randomColor();
+//     });
+//   document
+//     .getElementById("27")
+//     .addEventListener("click", function onClick(event) {
+//       event.target.style.backgroundColor = randomColor();
+//     });
+//   document
+//     .getElementById("28")
+//     .addEventListener("click", function onClick(event) {
+//       event.target.style.backgroundColor = randomColor();
+//     });
+//   document
+//     .getElementById("29")
+//     .addEventListener("click", function onClick(event) {
+//       event.target.style.backgroundColor = randomColor();
+//     });
+//   document
+//     .getElementById("30")
+//     .addEventListener("click", function onClick(event) {
+//       event.target.style.backgroundColor = randomColor();
+//     });
+// }
 // //let squareBrick = document.getElementsByClassName(".square"); // no diff
 // let squareBrick = document.getElementsByName("squarees"); // no diff
 // let vertiBrick = document.getElementsByClassName(".vertical");
