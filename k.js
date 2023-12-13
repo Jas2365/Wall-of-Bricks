5655c798cb4e314a5b48db2da00b50948694ebb5
Jas <jastha6wdc@gmail.com>
2023-12-13 16:28:02 +0530

    if in for in if in fn with arr

---
+++ k.js
@@ -0,0 +1,11 @@
+function k(value) {
+  let center = [8, 9, 10, 11, 14, 15, 16, 17, 20, 21, 22, 23];
+  if (value < 30) {
+    for (let i = 0; i < center.length; i++) {
+      if (value == center[i]) {
+        console.log("kk");
+      }
+    }
+  }
+}
+k(11);
